667b60170f6bbb3ab5c574e503ebcb020086fbf7
Angel <stanorma@cassiaschools.org>
2026-03-08 12:55:02 -0600

    Update server.js

--- server.js
+++ server.js
@@ -92,5 +92,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Game Server running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
